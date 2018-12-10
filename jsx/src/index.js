32e8dd6af96a3a8a88fd334c1ae8b661bd9d52bc
Ismail Cucevic <cucevicismail@gmail.com>
2018-12-10 13:05:25 +0000

    add jsx project

---
+++ jsx/src/index.js
@@ -0,0 +1,25 @@
+// Import the React and ReactDom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+const App = () => {
+        
+    const buttonText = {text: 'click me'};
+    const labelText = 'Enter Name';
+   
+    return (
+    <div>
+  <label className="label" htmlFor="name">{labelText}</label>
+  <input id="name" type="text"/>
+  <button style={{backgroundColor:'blue',color:'white'}}>{buttonText.text}</button>
+</div>);
+};
+
+
+//Take the react component and show it on the screen 
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+    );
+    
