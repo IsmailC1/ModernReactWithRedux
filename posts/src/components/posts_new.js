c9fd1f79e309b68d3301904e9a5fb8dd5f1d7c0f
Ismail Cucevic <cucevicismail@gmail.com>
2019-01-15 19:55:11 +0100

    save

--- posts/src/components/posts_new.js
+++ posts/src/components/posts_new.js
@@ -2,8 +2,24 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>Title</label>
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+  }
+
+  renderTagsField() {}
+
   render() {
-    return <div>PostsNew!</div>;
+    return (
+      <form>
+        <Field name="title" component={this.renderField} />
+        <Field name="tags" component={this.renderTagsField} />
+      </form>
+    );
   }
 }
 
